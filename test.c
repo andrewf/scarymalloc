56608fa77ed2006705093acf403942f28982134e
Andrew Fleenor <andrewflnr@gmail.com>
2014-04-29 21:47:13 -0700

    make sure malloc'd mem is actually writable without segfaulting

--- test.c
+++ test.c
@@ -1,12 +1,20 @@
 #include "stdlib.h"
 #include "stdio.h"
 
+void use(void* buf, size_t s) {
+    size_t i;
+    for(i=0; i<s; ++i) {
+        ((char*)buf)[i] = (char)(i%256);
+    }
+}
+
 int main(int argc, char** args) {
     void* ptrs[10];
     int i;
     printf("allocating.\n");
     for(i=0; i<10; ++i) {
         ptrs[i] = malloc(32);
+        use(ptrs[i], 32);
     }
     printf("freeing.\n");
     for(i=0; i<10; ++i) {
